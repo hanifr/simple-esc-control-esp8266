dcf29a470de7b27143a57d681afb500af3cd0573
hanifr <irhaniframli@gmail.com>
2023-01-30 18:19:19 +0800

    improve websocket connectivity

--- Websocket-Protocol/Client/Client.ino
+++ Websocket-Protocol/Client/Client.ino
@@ -5,120 +5,80 @@
 #include <ArduinoWebsockets.h>
 #include <WiFi.h>
 // #include <ESP8266WiFi.h>
-#include <WiFi.h>
 #include <Servo.h>
 
-const char* ssid = "SEA-IC"; //Enter SSID
-const char* password = "seaic2022"; //Enter Password
-const char* websockets_server_host = "192.168.1.111"; //Enter server adress
-const uint16_t websockets_server_port = 8888; // Enter server port
+const char* ssid = "SEA-IC"; 
+const char* password = "seaic2022"; 
+const char* websockets_server_host = "192.168.1.111"; 
+const uint16_t websockets_server_port = 8888; 
 
 using namespace websockets;
-int PWM;
 int ref_PWM;
 float PWM_CMD;
-static const int ESC_pin[2] = {2, 14};
+//static const int ESC_pin[2] = {2, 14};
+const int ESC_pin[2] = {2, 14};
 Servo ESC[2]; 
 
 WebsocketsClient client;
-bool wsConnected;
-void setup() {
-    Serial.begin(115200);
-    for(int i = 0; i < 2; ++i) {
-       if(!ESC[i].attach(ESC_pin[i], 1000, 2000)) {
-           Serial.print("Servo ");
-           Serial.print(i);
-           Serial.println("attach error");
-       }
-    }
-    delay(2000);
-    connectWiFi();
-    connectWebsocket();
-}
-void connectWiFi(){
-    WiFi.begin(ssid, password);
 
-    // Wait some time to connect to wifi
-    for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+void setup() {
+  Serial.begin(115200);
+  for (int i = 0; i < 2; ++i) {
+    if (!ESC[i].attach(ESC_pin[i], 1000, 2000)) {
+      Serial.print("Servo ");
+      Serial.print(i);
+      Serial.println("attach error");
+    }
+  }
+  delay(2000);
+  connectWiFi();
+  connectWebsocket();
+}
+
+void connectWiFi() {
+    WiFi.begin(ssid, password);
+    int i = 0;
+    while (WiFi.status() != WL_CONNECTED && i < 10) {
         Serial.print(".");
         delay(1000);
+        i++;
     }
-
-    // Check if connected to wifi
     if(WiFi.status() != WL_CONNECTED) {
         Serial.println("No Wifi!");
         return;
     }
-
     Serial.println("Connected to Wifi, Connecting to server.");
 }
-void onMessageCallback(WebsocketsMessage message) {
-    PWM = message.data().toInt();
-}
 
-void onEventsCallback(WebsocketsEvent event, String data) {
-    if(event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connnection Opened");
-        wsConnected = true;
-    } else if(event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connnection Closed");
-        wsConnected = false;
-    } else if(event == WebsocketsEvent::GotPing) {
-        Serial.println("Got a Ping!");
-    } else if(event == WebsocketsEvent::GotPong) {
-        Serial.println("Got a Pong!");
-    }
-}
-
-void connectWebsocket(){
-    // Connect to server
-    bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
-    if(connected) {
+void connectWebsocket() {
+    if (client.connect(websockets_server_host, websockets_server_port, "/")) {
         Serial.println("Connected!");
-        PWM = 0;
     } else {
         Serial.println("Not Connected!");
     }
-
-    // Setup Callbacks
-    client.onMessage(onMessageCallback);
-    client.onEvent(onEventsCallback);
-}
-
-bool pollWebsocket(){
-  if (client.available()){
-    client.poll(); // Get ready to receive next message
-    return true;  
-  }
-  else{
-    return false;
-  }
-}
-
-void readJoystick() {
-ref_PWM = PWM;
-
-PWM_CMD = map(ref_PWM, 0, 1023, 1100, 1900);
-}
-
-void updateMotor(){
-ESC[0].writeMicroseconds(PWM_CMD);
-ESC[1].writeMicroseconds(PWM_CMD);
+    client.onMessage([&](WebsocketsMessage message){
+//        Serial.print("Got Message: ");
+        Serial.println(message.data());
+        ref_PWM = atoi(message.data().c_str());
+        PWM_CMD = map(ref_PWM, 0, 1023, 1100, 1900);
+        ESC[0].writeMicroseconds(PWM_CMD);
+        ESC[1].writeMicroseconds(PWM_CMD);
+    });
 }
 
 void loop() {
+    if (WiFi.status() == WL_CONNECTED) {
+    if (client.available()) {
+      client.poll();
+    }
+  } else {
+    Exit();
+  }
+  delay(50);
+}
 
-if (WiFi.status() == WL_CONNECTED) {
-  if (pollWebsocket()) {
-      readJoystick();
-      updateMotor(); // move this line inside the "if (pollWebsocket())" block so the motor is updated only when new data is received
-    }
-  else {
-  connectWebsocket();
-    }
-  }
-else {
-  connectWiFi();
-  connectWebsocket();
-  }
+void Exit() {
+  WiFi.disconnect();
+  delay(500);
+  ESP.restart();
 }
--- Websocket-Protocol/Server/Server.ino
+++ Websocket-Protocol/Server/Server.ino
@@ -1,49 +1,63 @@
 
-#include <ArduinoWebsockets.h>
 #include <WiFi.h>
+#include <WebSocketsServer.h>
 
-const char* ssid = "ssid"; //Enter SSID
-const char* password = "password"; //Enter Password
+// Replace with your network credentials
+const char *ssid = "SEA-IC";
+const char *password = "seaic2022";
 
-using namespace websockets;
+WebSocketsServer webSocket = WebSocketsServer(8888);
 
-WebsocketsServer server;
+String JS;
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // Send message to client
+        webSocket.sendTXT(num, "Connected");
+      }
+      break;
+    case WStype_TEXT:
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+      // Send message to client
+      webSocket.sendTXT(num, JS);
+      break;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  // Connect to wifi
-  WiFi.begin(ssid, password);
 
-  // Wait some time to connect to wifi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
-      Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-  
+
+  Serial.println("Connected to WiFi");
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
 
-  server.listen(8888);
-  Serial.print("Is server live? ");
-  Serial.println(server.available());
+
+  Serial.println("WebSocket server started");
 }
 
 void loop() {
-  if (server.available()) {
-    WebsocketsClient client = server.accept();
-    while(client.connected()){
-      String msg = client.readString();
-      int JSTICK = analogRead(A0);
-      // log
-      Serial.print("Got Message: ");
-      Serial.println(msg);
-      client.send(String(JSTICK));
-    }
-    client.connect();
-  }
-  
-  delay(200);
+  // int JSTICK = random(1000,2000);
+  int JSTICK = analogRead(A0);
+  webSocket.loop();
+        JS = String(JSTICK);
+  webSocket.broadcastTXT(JS);
+//  delay(1000);
 }
 
